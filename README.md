77bb914cea21b5bad9ea94dbd08a866a204e1333
hanagatasachie <73112533+1543rrr@users.noreply.github.com>
2021-05-22 01:34:22 +0900

    Post モデルと posts テーブルを作成

--- README.md
+++ README.md
@@ -22,3 +22,27 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+## DB
+
+# user table
+|name   |string  |null :false|
+|email  |string  |null :false|
+|password|string |null :false|
+|password_confirmation|string|null :false|
+
+has_many: comments
+has_many: posts
+has_many: likes
+
+# posts table
+|title|string |null :false|
+|content|text|null :false|
+| user      | references | foreign_key :true |
+
+has_many: comments
+has_many: likes
+has_many: photos
+belongs_to :user
+
+# photos table
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_152309) do
+ActiveRecord::Schema.define(version: 2021_05_21_163144) do
+
+  create_table "posts", charset: "utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_05_21_152309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
