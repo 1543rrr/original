e41dae592929d97ba576be2def5e20c03f6184ba
hanagatasachie <73112533+1543rrr@users.noreply.github.com>
2021-05-22 19:10:49 +0900

    Post モデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :photos
   has_many :likes
   has_many :comments
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 500 }
+  
 end
